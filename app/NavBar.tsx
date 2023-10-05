603a53a169e4c7482a272c36aa65d2bcf69c73be
chronoflame78 <gaovanthet555@gmail.com>
2023-10-05 22:25:05 +0700

    add gg auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-200 p-5">
+    <div className="flex bg-slate-200 p-5 space-x-3">
       <Link href="/"><span className="mr-5">Home</span></Link>
       <Link href="/users"><span className="mr-5">Users</span></Link>
+      <Link href="/api/auth/signin"><span className="mr-5">Login</span></Link>
     </div>
   )
 }
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,18 +1,38 @@
-"use client"
-import { CldUploadWidget } from "next-cloudinary"
-import React from 'react'
+"use client";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+import React, { useState } from "react";
 
-const UploadPage = () => {
-  return (
-    <div>
-        <CldUploadWidget uploadPreset="zsspnoq0">
-            {({ open }) => 
-            <button 
-            onClick={() => open()}
-            className="btn btn-primary">Upload</button>}
-        </CldUploadWidget>
-    </div>
-  )
+interface CloudinaryResult {
+  public_id: string;
 }
 
-export default UploadPage
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
+  return (
+    <>
+      {!!publicId && <CldImage src={publicId} width={270} height={180} alt="" />}
+      <CldUploadWidget
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+        uploadPreset="zsspnoq0"
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles: 5,
+        }}
+      >
+        {({ open }) => (
+          <button onClick={() => open()} className="btn btn-primary">
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
