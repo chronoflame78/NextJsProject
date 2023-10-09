340100cef66ca0cda2daf7e576b573b1de6cd85c
chronoflame78 <gaovanthet555@gmail.com>
2023-10-09 22:37:52 +0700

    add session provider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,18 @@
+'use client'
+import { useSession } from "next-auth/react"
 import Link from "next/link"
 import React from 'react'
 
 const NavBar = () => {
+  const {status, data: session} = useSession();
+
   return (
     <div className="flex bg-slate-200 p-5 space-x-3">
       <Link href="/"><span className="mr-5">Home</span></Link>
       <Link href="/users"><span className="mr-5">Users</span></Link>
-      <Link href="/api/auth/signin"><span className="mr-5">Login</span></Link>
+      {status === 'loading' && <div>...Loading</div>}
+      {status === 'authenticated' && <div>{session.user!.name}</div>}
+      {status === 'unauthenticated' && <Link href="/api/auth/signin"><span className="mr-5">Login</span></Link>}
     </div>
   )
 }
